aaac2fe4d3cad2487d63b69e2b238dd6ec95695b
Eric Mendoza <eric.mendoza333@gmail.com>
2025-08-01 09:46:37 -0600

    feat(inertia): config inertia

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1 class="text-2xl font-bold">Star Wars Wiki</h1>
+    </div>
+</template>
+
---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="min-h-screen bg-gray-100 p-6">
+        <header class="text-xl font-semibold mb-4">Header</header>
+        <main><slot /></main>
+    </div>
+</template>
